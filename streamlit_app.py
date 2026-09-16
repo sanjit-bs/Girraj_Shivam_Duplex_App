5ef92923cf6e4761cd37e9737d6c7eb75e193df9
sanjit-bs <sanjitava.22260402016@cuj.ac.in>
2026-09-16 22:57:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 # import gspread
-from google.oauth2.service_account import Credentials
+# from google.oauth2.service_account import Credentials
 from io import BytesIO
 from datetime import date
 import datetime
